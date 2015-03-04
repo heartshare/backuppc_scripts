3658e22ec3d8dc8a65877907ebd4e5047ddd9ed0
root <root@bako@vip-consult.co.uk>
2015-03-04 22:50:03 +0000

    Fix the send email

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -19,21 +19,22 @@ if [ -z "$SERVER" ] || [ -z "$BACKUP_DIR" ] || [ -z "$CONTAINER" ]  ; then
 fi
 
 echo -e "Starting Mysql Docker backup conatiner with backup folder:$BACKUP_DIR , container:$CONTAINER , server: $SERVER ! \n"
-/usr/bin/ssh  -x -l backuppc $SERVER &&
-sudo docker run -it --rm --link $CONTAINER:$CONTAINER &&
--v /home/mysql/.my.cnf:/root/.my.cnf &&
--v /home/mysql_backup:/home/mysql_backup &&
--e backup_dir=$BACKUP_DIR/$SERVER/$CONTAINER &&
--e backup_server=$CONTAINER &&
--e backup_host=$SERVER  vipconsult/mysql_backup &&
+/usr/bin/ssh  -x -l vipconsult $SERVER \
+sudo docker run --rm --link $CONTAINER:$CONTAINER \
+-v /home/mysql/.my.cnf:/root/.my.cnf \
+-v /home/mysql_backup:/home/mysql_backup \
+-e backup_dir=$BACKUP_DIR/$SERVER/$CONTAINER \
+-e backup_server=$CONTAINER \
+-e backup_host=$SERVER  vipconsult/mysql_backup \
   2>  >(/usr/bin/tee -a ${DAILYLOGFILE} >&2)
 
 
 if [ -s $DAILYLOGFILE ]; then    #email only if error ocured - ie log file exists
-    cat "$DAILYLOGFILE"  | /usr/bin/mutt -s "Servers Backup Log" $EMAIL
+    cat "$DAILYLOGFILE" | /usr/bin/mail -s "Servers Backup Log" $EMAIL -aFrom:backuppc@vip-consult.co.uk
+
     if [[ $? -ne 0 ]]; then
         echo -e "ERROR:  Error log  could not be emailed to you! \n";
         else
-        echo -e "Backup Error log has been emailed to the admin! \n"
+        echo -e "Backup Error log has been emailed to $EMAIL ! \n"
     fi
 fi
